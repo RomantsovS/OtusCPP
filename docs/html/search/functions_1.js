726f833b18cb25937e7af1bfa7922ffe711ad3f3
RomantsovS <romantsov_s@rusklimat.ru>
2022-05-26 19:56:51 +0300

    removed all docs

--- docs/html/search/functions_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['print_5fip_0',['print_ip',['../main_8cpp.html#abe97cff6183f2aee8fd702d9c29dda41',1,'print_ip(T val):&#160;main.cpp'],['../main_8cpp.html#ad822990c9a8bec94025f5edf1dba0f47',1,'print_ip(tuple&lt; T, T, T, T &gt; val):&#160;main.cpp']]]
-];
